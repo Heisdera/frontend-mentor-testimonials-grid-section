71db6e844e78dacdf0b631e3331517386e6a98ff
Raphael Wisdom Chidera <123782431+Heisdera@users.noreply.github.com>
2024-01-09 02:35:21 +0100

    Update Quotation svg

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,7 @@ export default function App() {
               alt="daniel clifford"
               className="w-8 rounded-full"
             />
-            <img
-              src="../images/bg-pattern-quotation.svg"
-              alt="daniel clifford"
-              className="absolute right-[10%] top-0 w-28 rounded-full opacity-70"
-            />
+            <svg className="absolute right-[10%] top-0 w-28 rounded-full opacity-70" width="104" height="102" xmlns="http://www.w3.org/2000/svg"><path d="M104 102V59.727H84.114c0-5.871.689-11.182 2.068-15.933 1.379-4.75 3.42-9.287 6.125-13.61C95.01 25.86 98.909 22.257 104 19.375V0c-9.758 4.27-17.712 9.874-23.864 16.813-6.151 6.939-10.712 14.545-13.681 22.818C63.485 47.904 62 59.941 62 75.74V102h42zm-62 0V59.727H22.114c0-5.871.689-11.182 2.068-15.933 1.379-4.75 3.42-9.287 6.125-13.61C33.01 25.86 36.909 22.257 42 19.375V0c-9.652 4.27-17.58 9.874-23.784 16.813C12.01 23.752 7.424 31.358 4.455 39.631 1.485 47.904 0 59.941 0 75.74V102h42z" fill="#A775F1" fill-rule="nonzero"/></svg>
 
             <div className="leading-4">
               <h2 className="text-custom font-semibold">Daniel Clifford</h2>
